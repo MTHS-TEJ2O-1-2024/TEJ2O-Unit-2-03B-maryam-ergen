2ae547d4fd2cc8636d7314f17435049296013278
maryam-ergen <maryam.ergen@mths.ca>
2024-09-17 11:15:30 -0400

    updated code and comment

--- main.ts
+++ main.ts
@@ -2,7 +2,15 @@
  *
  * Created by: Maryam Ergen
  * Created on: Sep 2024
- * This program displays Hello, Maryam!
+ * This program displays area and perimeter calculations
 */
 
-basic.showString('Hello, Maryam!')
+
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('5 + 5 + 3 + 3 = ' + (5 + 5 + 3 + 3).toString())
+
+basic.pause (500)
+
+basic.showString('5 * 3 = ' + ( 5 * 3).toString())
